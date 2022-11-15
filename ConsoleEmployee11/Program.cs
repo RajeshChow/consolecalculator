13e62bb6d87a0d78d5297338707e85b0c104891e
RajeshChow <117570513+RajeshChow@users.noreply.github.com>
2022-11-15 13:58:04 +0530

    Add files via upload

---
+++ ConsoleEmployee11/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleEmployee11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class34 s=new Class34();
+            s.EmployeeDetails();
+        }
+    }
+}
